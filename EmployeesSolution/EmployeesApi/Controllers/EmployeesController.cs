61c9dbaed15695104c5d82ea71a07d6015f704eb
jayehill <jayehill.sbcglobal.net>
2023-03-21 10:27:07 -0400

    day2

---
+++ EmployeesSolution/EmployeesApi/Controllers/EmployeesController.cs
@@ -0,0 +1,11 @@
+﻿namespace EmployeesApi.Controllers;
+
+public class EmployeesController : ControllerBase
+
+{
+    // GET /employees     [HttpGet("/employees")]
+    public async Task<ActionResult> GetAllEmployees()
+    {
+        return Ok();
+    }
+}
